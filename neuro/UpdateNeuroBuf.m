4a47ebdb6435184b7d441a7ab5e0498f1a5dfce4
bravo experimenter <sarahseko@gmail.com>
2022-08-19 12:27:34 -0700

    udpated neurobuf

--- neuro/UpdateNeuroBuf.m
+++ neuro/UpdateNeuroBuf.m
@@ -2,9 +2,10 @@ function Neuro = UpdateNeuroBuf(Neuro)
 % Neuro = UpdateNeuroBuf(Neuro)
 % efficiently replaces old data in circular buffer with new filtered
 % signals
-samps = size(Neuro.BroadbandData{end},1);
 
-Neuro.DataBuf = circshift(Neuro.DataBuf,-samps,2);
-Neuro.DataBuf(:,(end-samps+1):end) = Neuro.BroadbandData{end}';
+
+samps = Neuro.NumSamps;
+Neuro.FilterDataBuf = circshift(Neuro.FilterDataBuf,-samps);
+Neuro.FilterDataBuf((end-samps+1):end,:,:) = Neuro.FilteredData(:,:,1:Neuro.NumBuffer);
 
 end % UpdateNeuroBuf
