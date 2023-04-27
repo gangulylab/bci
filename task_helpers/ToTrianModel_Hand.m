c259e8117fad19a64241299165a7ae0b30310bb5
NIKHILESH NATRAJ <nikhilesh.natraj@ucsf.edu>
2023-04-26 23:35:22 -0700

    Create ToTrianModel_Hand.m

---
+++ task_helpers/ToTrianModel_Hand.m
@@ -0,0 +1,180 @@
+%% CREATING A MLP FOR B3, NO POOLING
+
+clc;clear
+close all
+
+
+
+% IMAGINED 
+clc;clear
+root_path = '/home/ucsf/Data/Bravo3/20230330/Robot3DArrow';
+foldernames = {'122117', '123029', '124354', '125220','125810'};
+cd(root_path)
+
+
+D1=[];
+D2=[];
+D3=[];
+D4=[];
+D5=[];
+D6=[];
+D7=[];
+D8=[];
+D9=[];
+D10=[];
+D11=[];
+D12=[];
+for i=1:length(foldernames)
+    folderpath = fullfile(root_path, foldernames{i},'Imagined');
+    D=dir(folderpath);
+    for j=3:length(D)
+        filepath=fullfile(folderpath,D(j).name);
+        load(filepath)
+        features  = TrialData.SmoothedNeuralFeatures;
+        %kinax = length(features)-20:length(features);
+        kinax = find(TrialData.TaskState==3);
+        temp = cell2mat(features(kinax));
+
+        % get delta, beta and hG removing bad channels 
+        temp = temp([257:512 1025:1280 1537:1792],:);        
+        bad_ch = [108 113 118];
+        good_ch = ones(size(temp,1),1);
+        for ii=1:length(bad_ch)
+            bad_ch_tmp = bad_ch(ii)*[1 2 3];
+            good_ch(bad_ch_tmp)=0;
+        end
+        temp = temp(logical(good_ch),:);
+
+        if TrialData.TargetID == 1
+            D1 = [D1 temp];
+        elseif TrialData.TargetID == 2
+            D2 = [D2 temp];
+        elseif TrialData.TargetID == 3
+            D3 = [D3 temp];
+        elseif TrialData.TargetID == 4
+            D4 = [D4 temp];
+        elseif TrialData.TargetID == 5
+            D5 = [D5 temp];
+        elseif TrialData.TargetID == 6
+            D6 = [D6 temp];
+        elseif TrialData.TargetID == 7
+            D7 = [D7 temp];
+        elseif TrialData.TargetID == 8
+            D8 = [D8 temp];
+        elseif TrialData.TargetID == 9
+            D9 = [D9 temp];
+        elseif TrialData.TargetID == 10
+            D10 = [D10 temp];
+        elseif TrialData.TargetID == 11
+            D11 = [D11 temp];
+        elseif TrialData.TargetID == 12
+            D12 = [D12 temp];
+        end
+    end
+end
+
+
+% ONLINE DATA AS WELL
+root_path = '/home/ucsf/Data/Bravo3/20230330/Robot3DArrow';
+foldernames = {'130524', '131124', '131630', '132104','132647','133231'};
+cd(root_path)
+
+for i=1:length(foldernames)
+    folderpath = fullfile(root_path, foldernames{i},'BCI_Fixed');
+    D=dir(folderpath);
+    for j=3:length(D)
+        filepath=fullfile(folderpath,D(j).name);
+        load(filepath)
+        features  = TrialData.SmoothedNeuralFeatures;
+        kinax = find(TrialData.TaskState==3);
+        temp = cell2mat(features(kinax));
+
+        % get delta, beta and hG removing bad channels
+        temp = temp([257:512 1025:1280 1537:1792],:);
+        bad_ch = [108 113 118];
+        good_ch = ones(size(temp,1),1);
+        for ii=1:length(bad_ch)
+            bad_ch_tmp = bad_ch(ii)*[1 2 3];
+            good_ch(bad_ch_tmp)=0;
+        end
+        temp = temp(logical(good_ch),:);
+
+        if TrialData.TargetID == 1
+            D1 = [D1 temp];
+        elseif TrialData.TargetID == 2
+            D2 = [D2 temp];
+        elseif TrialData.TargetID == 3
+            D3 = [D3 temp];
+        elseif TrialData.TargetID == 4
+            D4 = [D4 temp];
+        elseif TrialData.TargetID == 5
+            D5 = [D5 temp];
+        elseif TrialData.TargetID == 6
+            D6 = [D6 temp];
+        elseif TrialData.TargetID == 7
+            D7 = [D7 temp];
+        elseif TrialData.TargetID == 8
+            D8 = [D8 temp];
+        elseif TrialData.TargetID == 9
+            D9 = [D9 temp];
+        elseif TrialData.TargetID == 10
+            D10 = [D10 temp];
+        elseif TrialData.TargetID == 11
+            D11 = [D11 temp];
+        elseif TrialData.TargetID == 12
+            D12 = [D12 temp];
+        end
+    end
+end
+
+
+
+
+clear condn_data
+idx=1;
+condn_data{1}=[D1(idx:end,:) ]'; 
+condn_data{2}= [D2(idx:end,:)]'; 
+condn_data{3}=[D3(idx:end,:)]'; 
+condn_data{4}=[D4(idx:end,:)]'; 
+condn_data{5}=[D5(idx:end,:)]'; 
+condn_data{6}=[D6(idx:end,:)]'; 
+condn_data{7}=[D7(idx:end,:)]'; 
+condn_data{8}=[D8(idx:end,:)]'; 
+condn_data{9}=[D9(idx:end,:)]'; 
+condn_data{10}=[D10(idx:end,:)]'; 
+condn_data{11}=[D11(idx:end,:)]'; 
+condn_data{12}=[D12(idx:end,:)]'; 
+
+
+N=[];
+T1=[];
+for i=1:length(condn_data)
+    tmp=condn_data{i};
+    N = [N tmp'];
+    T1 = [T1;i*ones(size(tmp,1),1)];
+end
+
+
+T = zeros(size(T1,1),length(condn_data));
+for i=1:length(condn_data)
+    [aa bb]=find(T1==i);
+    T(aa(1):aa(end),i)=1;
+end
+
+
+%%%% CODE TO TRAIN A NEURAL NETWORK FROM SCRATCH
+clear net
+net = patternnet([64 64 ]) ;
+net.performParam.regularization=0.2;
+net = train(net,N,T','UseParallel','yes');
+cd('/home/ucsf/Projects/bci/clicker')
+genFunction(net,'MLP_Hand_04272023_CL1_NoPooling')
+%%%%%%%%%%%%%%%%%%%%%%% END SECTION %%%%%
+
+
+cd('/home/ucsf/Projects/bci')
+clear
+%ExperimentStart('Robot3DArrow','Bravo3',4,1,0)
+ExperimentStart('HandOnline','Bravo3',4,1,0)
+
+
