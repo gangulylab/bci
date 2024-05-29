52e5c25bcd826e90d6facd468a9ecae888ab0dca
NIKHILESH NATRAJ <nikhilesh.natraj@ucsf.edu>
2024-05-29 22:17:56 +0530

    4DofTrainingCode

--- task_helpers/ToTrianModel_4DoF.m
+++ task_helpers/ToTrianModel_4DoF.m
@@ -177,10 +177,10 @@ end
 clear condn_data
 % combing both onlien plus offline
 idx=1;
-condn_data{1}=[D1(idx:end,:) ]'; % act1
-condn_data{2}= [D2(idx:end,:)]'; % act2
-condn_data{3}=[D3(idx:end,:)]'; % act3
-condn_data{4}=[D4(idx:end,:)]'; % act4
+condn_data{1}=[D1(idx:end,:) ]'; % right thumb
+condn_data{2}= [D2(idx:end,:)]'; % both feet
+condn_data{3}=[D3(idx:end,:)]'; % left pinch
+condn_data{4}=[D4(idx:end,:)]'; % head
 % condn_data{5}=[D5(idx:end,:)]'; % lips
 % condn_data{6}=[D6(idx:end,:)]'; % tong
 % condn_data{7}=[D7(idx:end,:)]'; % both hands
@@ -195,8 +195,7 @@ D = condn_data{4};
 % G= condn_data{7};
 
 clear N
-%N = [A' B' C' D' E' F' G'];
-N = [A' B' C' D'];
+N = [A' B' C' D' ];
 T1 = [ones(size(A,1),1);2*ones(size(B,1),1);3*ones(size(C,1),1);4*ones(size(D,1),1)];
 T = zeros(size(T1,1),4);
 [aa bb]=find(T1==1);[aa(1) aa(end)]
@@ -207,38 +206,38 @@ T(aa(1):aa(end),2)=1;
 T(aa(1):aa(end),3)=1;
 [aa bb]=find(T1==4);[aa(1) aa(end)]
 T(aa(1):aa(end),4)=1;
-[aa bb]=find(T1==5);[aa(1) aa(end)]
-T(aa(1):aa(end),5)=1;
-[aa bb]=find(T1==6);[aa(1) aa(end)]
-T(aa(1):aa(end),6)=1;
-[aa bb]=find(T1==7);[aa(1) aa(end)]
-T(aa(1):aa(end),7)=1;
+% [aa bb]=find(T1==5);[aa(1) aa(end)]
+% T(aa(1):aa(end),5)=1;
+% [aa bb]=find(T1==6);[aa(1) aa(end)]
+% T(aa(1):aa(end),6)=1;
+% [aa bb]=find(T1==7);[aa(1) aa(end)]
+% T(aa(1):aa(end),7)=1;
 
 %%%%%%%% CODE TO UPDATE THE PNP DECODER %%%%%%%
 cd('/home/ucsf/Projects/bci/clicker')
-load net_7DoF_New_HeadLips_Replaced % or use net_7DoF_New_Head_Replaced
-net = net_7DoF_New_HeadLips_Replaced; % or use net_7DoF_New_Head_Replaced
-net.divideParam.trainRatio=0.80;
-net.divideParam.valRatio=0.1;
-net.divideParam.testRatio=0.1;
-net = train(net,N,T','UseParallel','no');
-% use an appropriate name and update GetParams NeuralNetFunction
-genFunction(net,'MLP_7Dir_B3_PnP_04042023_NoPooling_Update2') ;
-net_mlp_new7_update1 = net;
-save net_mlp_new7_update1 net_mlp_new7_update1
+% load net_7DoF_New_HeadLips_Replaced % or use net_7DoF_New_Head_Replaced
+% net = net_7DoF_New_HeadLips_Replaced; % or use net_7DoF_New_Head_Replaced
+% net.divideParam.trainRatio=0.80;
+% net.divideParam.valRatio=0.1;
+% net.divideParam.testRatio=0.1;
+% net = train(net,N,T','UseParallel','no');
+% % use an appropriate name and update GetParams NeuralNetFunction
+% genFunction(net,'MLP_7Dir_B3_PnP_04042023_NoPooling_Update2') ;
+% net_mlp_new7_update1 = net;
+% save net_mlp_new7_update1 net_mlp_new7_update1
 %%%%%%%%%%%%% END SECTION %%%%%%%%%%%%
 
 % 
 % % %%%% CODE TO TRAIN A NEURAL NETWORK FROM SCRATCH
-% clear net
-% net = patternnet([120]);
-% net.performParam.regularization=0.2;
-% net.divideParam.trainRatio=0.80;
-% net.divideParam.valRatio=0.10;
-% net.divideParam.testRatio=0.1;
-% net = train(net,N,T','UseParallel','no');
-% cd('/home/ucsf/Projects/bci/clicker')
-% genFunction(net,'MLP_7Dir_B3_20231117_CL2_NoPooling')
+clear net
+net = patternnet([120]);
+net.performParam.regularization=0.2;
+net.divideParam.trainRatio=0.80;
+net.divideParam.valRatio=0.10;
+net.divideParam.testRatio=0.1;
+net = train(net,N,T','UseParallel','no');
+cd('/home/ucsf/Projects/bci/clicker')
+genFunction(net,'MLP_7Dir_B3_20231117_CL2_NoPooling')
 % save net net
 % %%%%%%%%%%%%%%%%%%%%%%% END SECTION %%%%%
 % 
